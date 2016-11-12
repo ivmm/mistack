5b0b6652bd02ca3517eac10d99d21437742e645b
ivmm <ivmmvobe@mf8.biz>
2016-11-12 13:22:17 +0800

    fix

--- include/nginx.sh
+++ include/nginx.sh
@@ -52,7 +52,7 @@ Install_Nginx() {
   --with-openssl=../openssl-$openssl_version \
   --with-ld-opt="-ljemalloc" \
   --add-module=../nginx-ct-${nginx_ct_version} \
-  --add-module=../ngx_brotli_$ngx_brotli_version
+  --add-module=../ngx_brotli-$ngx_brotli_version
   make -j ${THREAD} && make install
   if [ -e "$nginx_install_dir/conf/nginx.conf" ]; then
     popd 
